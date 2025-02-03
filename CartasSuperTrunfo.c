e4dea9b0d84d785b2530d44bdab11bc6eb9dd524
Leandro Pinheiro Munhoz da Silva <lepms@outlook.com>
2025-02-03 23:01:48 +0000

    Configurando Printf e Scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,11 +9,22 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    int código da cidade;
+    char nome [50];
+    float população;
+    float área;
+    float PIB;
+    int número de pontos turísticos;
+
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
+    printf("Entre com o código da cidade\n");
+    scanf("Código da cidade: %d");
+    
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
